7a99833f03a19b9e6cc73ff93518b789522f3160
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2022-06-22 13:23:30 +0530

    form component completed

--- 03_Vue-forms/src/components/SignUpForm.vue
+++ 03_Vue-forms/src/components/SignUpForm.vue
@@ -1,13 +1,23 @@
+<!--
+    We work 2 way for check boxes if its a single value check box then boolean
+    is used,
+    else when we have multiple choise multiple answers type of thing then
+    we use value attribute on each input (type- checkbox) and
+    then we use array to maintain which values are checked.
+-->
 <template>
-  <form>
+  <form @submit.prevent="submitHandler">
     <div class="inputPair">
       <label for="q1">Email : </label>
       <input type="email" required v-model="email" id="q1" class="input" />
     </div>
+
     <div class="inputPair">
       <label for="q2">Password : </label>
       <input type="password" required v-model="psswd" id="q2" class="input" />
+      <p v-if="psswdError" class="Error">{{ psswdError }}</p>
     </div>
+
     <div class="inputPair">
       <label for="q3">Role : </label>
       <select id="q3" v-model="role" class="input">
@@ -17,6 +27,7 @@
         <option value="vueDeveloper">vueDeveloper</option>
       </select>
     </div>
+
     <div class="inputPair">
       <label>Skills</label>
       <input
@@ -26,44 +37,33 @@
         @keypress="addSkill"
       />
     </div>
-    <div>
+
+    <div class="skills">
+      <div
+        v-for="skill in skills"
+        :key="skill"
+        @click="deleteSkill"
+        :id="skill"
+        class="pill"
+      >
+        {{ skill }}
+      </div>
+    </div>
+
+    <div class="terms">
       <input type="checkbox" required v-model="terms" id="q4" />
       <label for="q4">Accept Terms and Conditions</label>
     </div>
-    <!--
-        We work 2 way for check boxes if its a single value check box then boolean
-        is used,
-        else when we have multiple choise multiple answers type of thing then
-        we use value attribute on each input (type- checkbox) and
-        then we use array to maintain which values are checked.
-    -->
-    <!-- <div>
-      <input type="checkbox" v-model="names" value="bhavya" />
-      <label>bhavya</label>
-      <input type="checkbox" v-model="names" value="utkris" />
-      <label>utkris</label>
-      <input type="checkbox" v-model="names" value="kb" />
-      <label>kb</label>
-      <input type="checkbox" v-model="names" value="abisek" />
-      <label>abisek</label>
-    </div> -->
+
+    <div class="submit">
+      <button type="submit">Create an Account</button>
+    </div>
   </form>
   {{ email }}
   {{ psswd }}
   {{ role }}
   {{ terms }}
-  <!-- <ul>
-    <li v-for="n in names" :key="n">
-      {{ n }}
-    </li>
-  </ul> -->
   {{ tempSkill }}
-  <!-- {{ skills }} -->
-  <ul>
-    <li v-for="skill in skills" :key="skill" @click="deleteSkill" :id="skill">
-      {{ skill }}
-    </li>
-  </ul>
 </template>
 
 <script>
@@ -74,9 +74,9 @@ export default {
       psswd: "",
       role: "designer",
       terms: false,
-      //   names: [],
       tempSkill: "",
       skills: ["bhavya", "html", "css", "js"],
+      psswdError: "",
     };
   },
   methods: {
@@ -93,6 +93,20 @@ export default {
     deleteSkill(e) {
       this.skills = this.skills.filter((s) => s !== e.target.id);
     },
+    submitHandler(e) {
+      if (this.psswd.length <= 5) {
+        this.psswdError = "Password must be atleast 6 chars long";
+        return;
+      } else {
+        this.psswdError = "";
+      }
+      if (!this.psswdError) {
+        console.log("submitted");
+        this.email = "";
+        this.psswd = "";
+        this.terms = false;
+      }
+    },
   },
 };
 </script>
@@ -102,6 +116,7 @@ form {
   width: 60%;
   margin: auto;
   padding: 4rem;
+  padding-bottom: 1rem;
 }
 .inputPair {
   margin-bottom: 1rem;
@@ -117,5 +132,35 @@ form {
   max-height: 1.6rem;
   padding: 3px 10px;
   background-color: #eef;
+}
+.pill {
+  display: inline-block;
+  background-color: pink;
+  padding: 5px 5px;
+  border-radius: 1rem;
+  cursor: default;
+}
+.terms {
+  display: flex;
+  gap: 0.5rem;
+  margin-top: 1rem;
+}
+.skills {
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+.Error {
+  color: red;
+  margin-top: 0;
+  font-size: 0.8rem;
+  margin-bottom: 0;
+}
+button {
+  padding: 1rem;
+  border-radius: 1rem;
+  margin-top: 1rem;
+  background: #48cae4;
+  color: white;
 }
 </style>
